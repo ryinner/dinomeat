8fc8db0a22ac51e47988898ed6a0386dfab9f87c
ryinner <vinograd18062002@gmail.com>
2023-09-25 15:46:34 +0500

    comment orm type errors

--- services/orm/users.service.ts
+++ services/orm/users.service.ts
@@ -1,9 +1,9 @@
-import { prisma } from '../lib/prisma.service';
+// import { prisma } from '../lib/prisma.service';
 
-export async function getUserById (id: number) {
-  return await prisma.user.findUniqueOrThrow({
-    where: {
-      id
-    }
-  });
-}
+// export async function getUserById (id: number) {
+//   return await prisma.user.findUniqueOrThrow({
+//     where: {
+//       id
+//     }
+//   });
+// }
