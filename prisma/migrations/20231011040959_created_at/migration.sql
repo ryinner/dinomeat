5db244e835f8d62c8d0608c4f14df26dd51a43b1
ryinner <vinograd18062002@gmail.com>
2023-10-11 10:22:28 +0500

    remove migrations from repo

--- prisma/migrations/20231011040959_created_at/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `created_at` on table `users` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `users` MODIFY `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0);
