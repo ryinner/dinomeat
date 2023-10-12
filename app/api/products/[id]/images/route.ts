28d378174c2558f7a8a31dfa38b44ae707689362
ryinner <vinograd18062002@gmail.com>
2023-10-12 17:08:45 +0500

    fix unique

--- app/api/products/[id]/images/route.ts
+++ app/api/products/[id]/images/route.ts
@@ -1,3 +1,4 @@
+import { PrismaErrorsTypes, getPrismaErrorType, isPrismaError } from '@/services/lib/prisma.service';
 import { getProductImagesPath, writeFile } from '@/services/orm/images.service';
 import { updateProduct } from '@/services/orm/products.service';
 import { NextRequest, NextResponse } from 'next/server';
@@ -12,7 +13,7 @@ export async function POST (req: NextRequest, { params }: { params: RouteRequire
   const alt = formData.get('alt')?.toString();
 
   if (!(file instanceof File)) {
-    return NextResponse.json({ status: 400, message: 'file is required' });
+    return NextResponse.json({ status: 400, message: 'картинка обязательна' });
   }
 
   if (!alt || alt?.trim() === '') {
@@ -21,18 +22,30 @@ export async function POST (req: NextRequest, { params }: { params: RouteRequire
 
   const fileInfo = await writeFile(file, getProductImagesPath(params.id));
 
-  await updateProduct(Number(params.id), {
-    images: {
-      create: {
-        image: {
-          create: {
-            alt,
-            url: fileInfo.filepath
+  try {
+    await updateProduct(Number(params.id), {
+      images: {
+        create: {
+          image: {
+            create: {
+              alt,
+              url: fileInfo.filepath
+            }
           }
         }
       }
+    });
+  } catch (error) {
+    if (isPrismaError(error)) {
+      const errorType = getPrismaErrorType(error);
+      switch (errorType) {
+        case PrismaErrorsTypes.unique:
+          return NextResponse.json({ status: 400, message: 'Картинка с таким именем уже загружена' });
+        default:
+          return NextResponse.json({ status: 400, message: 'Неизвестная ошибка БД' });
+      }
     }
-  });
+  }
 
   return NextResponse.json({ status: 200, fileInfo });
 }
--- services/lib/prisma.service.ts
+++ services/lib/prisma.service.ts
@@ -1,5 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+export const prisma = new PrismaClient()
 
-export { prisma };
+export function isPrismaError (error: unknown): error is Prisma.PrismaClientKnownRequestError {
+  return error instanceof Prisma.PrismaClientKnownRequestError;
+}
+
+export enum PrismaErrorsTypes {
+  unknown,
+  unique
+}
+
+export function getPrismaErrorType (error: Prisma.PrismaClientKnownRequestError): PrismaErrorsTypes {
+  if (error.code === 'P2002') {
+    return PrismaErrorsTypes.unique;
+  }
+  return PrismaErrorsTypes.unknown;
+}
