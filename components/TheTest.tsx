c3bc118bdc16b450adb609aa250427d71e78f248
ryinner <vinograd18062002@gmail.com>
2023-10-12 13:40:22 +0500

    upload images

--- components/TheTest.tsx
+++ components/TheTest.tsx
@@ -1,6 +1,9 @@
+// import Image from 'next/image';
+
 export function TheTest () {
   return <form name='test' method='POST' action='/api/products/1/images' encType="multipart/form-data">
     <input type='file' name='image' required />
     <button type='submit'>Save</button>
+    {/* <Image width={500} height={500} src='/resources/products/1/1.jpg' alt='123'/> */}
   </form>
 }
