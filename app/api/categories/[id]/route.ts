cf0861881fc522f9e7e962339f4d2a4db5be287e
ryinner <vinograd18062002@gmail.com>
2023-10-16 10:10:21 +0500

    fixes

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -1,6 +1,14 @@
 import { updateCategory } from '@/services/orm/categories.service';
 import { NextRequest, NextResponse } from 'next/server';
 
+export async function PUT (req: NextRequest, params: Params)  {
+  const data = await req.json() as InputsPut;
+
+  const category = await updateCategory(Number(params.id), data);
+
+  return NextResponse.json({ status: 200, message: 'Обновлено', category });
+}
+
 interface Params {
   id: number | string;
 }
@@ -8,12 +16,4 @@ interface Params {
 interface InputsPut {
   id: number;
   name?: string;
-}
-
-export async function PUT (req: NextRequest, params: Params)  {
-  const data = await req.json() as InputsPut;
-
-  const category = await updateCategory(Number(params.id), data);
-
-  return NextResponse.json({ status: 200, message: 'Обновлено', category });
 }
--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,14 +1,14 @@
 import { createCategory } from '@/services/orm/categories.service';
 import { NextRequest, NextResponse } from 'next/server';
 
-interface InputsPost {
-  name: string;
-}
-
 export async function POST (req: NextRequest) {
   const data = await req.json() as InputsPost;
 
   const category = await createCategory(data);
 
   return NextResponse.json({ code: 200, message: 'Создано', category });
+}
+
+interface InputsPost {
+  name: string;
 }
