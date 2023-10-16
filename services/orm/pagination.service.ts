66019fa6738bfe4517676d670ab9441d15c6a909
ryinner <vinograd18062002@gmail.com>
2023-10-16 11:16:20 +0500

    pagination service

---
+++ services/orm/pagination.service.ts
@@ -0,0 +1,41 @@
+interface PaginationSettings<T> {
+  limit: number;
+  page: number;
+  count: number;
+  maxPages?: number;
+}
+
+export async function pagination <T>({ page, limit, count, maxPages = 11 }: PaginationSettings<T>) {
+  const last = Math.round(count / limit);
+  const first = 1;
+
+  let start = first;
+  let end!: number;
+
+  if (last > maxPages) {
+    const middle = Math.floor(maxPages / 2);
+    const leftBound = page - middle;
+    const rightBound = page + middle;
+
+    start = (leftBound < 1 ? 1 : leftBound) - (rightBound > last ? rightBound - last : 0);
+    end = (rightBound > last ? last : rightBound) + (leftBound < 1 ? 1 - leftBound : 0);
+
+    if (start != 1) {
+      start += 2;
+    }
+    if (end != last) {
+      end -= 2;
+    }
+  }
+
+  return {
+    page,
+    last,
+    first,
+    start,
+    end,
+    previous: page > first ? page - 1 : first,
+    next: page < last ? page + 1 : last,
+    total: count,
+  }
+}
