532a8fd9e5765f4ec09e567ec40c2b1a0d514218
ryinner <vinograd18062002@gmail.com>
2023-11-03 11:44:34 +0500

    editor menu to subcomponent

--- components/Controls/ControlsEditor.tsx
+++ components/Controls/ControlsEditor.tsx
@@ -1,6 +1,7 @@
 import TextStyle from "@tiptap/extension-text-style";
 import {
   BubbleMenu,
+  Editor,
   EditorContent,
   FloatingMenu,
   useEditor,
@@ -29,62 +30,71 @@ export default function ControlsEditor({ value, onInput }: Props) {
 
   return (
     <>
-      {editor && (
-        <>
-          <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
-            {headingLevels.map((hl) => (
-              <button
-                type='button'
-                key={hl}
-                onClick={() =>
-                  editor.chain().focus().toggleHeading({ level: hl }).run()
-                }
-                className={
-                  editor.isActive("heading", { level: hl }) ? "is-active" : ""
-                }
-              >
-                {`h${hl}`}
-              </button>
-            ))}
-            <button
-              type='button'
-              onClick={() => editor.chain().focus().toggleBulletList().run()}
-              className={editor.isActive("bulletList") ? "is-active" : ""}
-            >
-              * список
-            </button>
-          </FloatingMenu>
-          <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
-            <button
-              type='button'
-              onClick={() => editor.chain().focus().toggleBold().run()}
-              className={editor.isActive("bold") ? "is-active" : ""}
-            >
-              bold
-            </button>
-            <button
-              type='button'
-              onClick={() => editor.chain().focus().toggleItalic().run()}
-              className={editor.isActive("italic") ? "is-active" : ""}
-            >
-              italic
-            </button>
-            <button
-              type='button'
-              onClick={() => editor.chain().focus().toggleStrike().run()}
-              className={editor.isActive("strike") ? "is-active" : ""}
-            >
-              strike
-            </button>
-          </BubbleMenu>
-        </>
-      )}
+      {editor && <MenuBar editor={editor} />}
       <EditorContent editor={editor} />
     </>
   );
 }
 
-function MenuBar() {}
+function MenuBar({ editor }: { editor: Editor }) {
+  return (
+    <>
+      <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
+        {headingLevels.map((hl) => (
+          <button
+            type="button"
+            key={hl}
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: hl }).run()
+            }
+            className={
+              editor.isActive("heading", { level: hl }) ? "is-active" : ""
+            }
+          >
+            {`h${hl}`}
+          </button>
+        ))}
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") ? "is-active" : ""}
+        >
+          * список
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("bulletList") ? "is-active" : ""}
+        >
+          1. список
+        </button>
+      </FloatingMenu>
+      <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive("bold") ? "is-active" : ""}
+        >
+          bold
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive("italic") ? "is-active" : ""}
+        >
+          italic
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={editor.isActive("strike") ? "is-active" : ""}
+        >
+          strike
+        </button>
+      </BubbleMenu>
+    </>
+  );
+}
 
 interface Props {
   value: string;
