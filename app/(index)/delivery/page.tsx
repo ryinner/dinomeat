9454458a51b7b7ba9da0bc2057338e5a5681a4d9
ryinner <vinograd18062002@gmail.com>
2023-11-14 17:38:56 +0500

    delivery page

---
+++ app/(index)/delivery/page.tsx
@@ -0,0 +1,23 @@
+import styles from './page.module.scss';
+
+export default function Delivery() {
+  return (
+    <section className={styles.delivery}>
+      <h1 className={styles.delivery__heading}>Доставка</h1>
+      <div className={styles.delivery__info}>
+        <p className={styles.delivery__paragraph}>
+          Доставка осуществляется в постаматы и пункты выдачи заказов СДЭК в
+          срок от 1 дня от даты совершения заказа.
+        </p>
+        <p className={styles.delivery__paragraph}>
+          Товар доставляется в постаматы и пункты выдачи заказов СДЭК при
+          наличии полной оплаты за товар и доставку. Стоимость доставки
+          составляет 200 рублей. Заказ можно оформить на сайте компании ДНС. Вес
+          заказа не может быть более 20кг. При поступлении заказа в постамат /
+          пункт выдачи заказов, Вам поступит смс оповещение от компании СДЭК с
+          кодом для получения.
+        </p>
+      </div>
+    </section>
+  );
+}
