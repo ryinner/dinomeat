9c1459d202752299e7efbc0609b1fc9e5a1e14ff
ryinner <vinograd18062002@gmail.com>
2023-11-14 17:50:53 +0500

    page components

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,5 +1,11 @@
+import TheAboutUs from '@/components/Index/TheAboutUs';
+import TheEmployers from '@/components/Index/TheEmployers';
 import TheHero from "@/components/Index/TheHero";
 
 export default function Index() {
-  return <TheHero />;
+  return <>
+    <TheHero />
+    <TheEmployers />
+    <TheAboutUs />
+  </>;
 }
