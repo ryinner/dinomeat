73b646c3f41e7fab908c4c869e9f90ecf21d76e5
ryinner <vinograd18062002@gmail.com>
2023-11-15 10:26:27 +0500

    about us

--- components/Index/TheAboutUs.tsx
+++ components/Index/TheAboutUs.tsx
@@ -1,11 +1,13 @@
+import styles from './TheAboutUs.module.scss';
+
 export default function TheAboutUs() {
   return (
-    <section>
-      <p>
+    <section className={styles['about-us']}>
+      <p className={styles['about-us__paragraph']}>
         Добро пожаловать в нашу компанию, специализирующуюся на продаже одежды
         для скейтеров!
       </p>
-      <p>
+      <p className={styles['about-us__paragraph']}>
         Мы являемся поклонниками этого захватывающего экстремального спорта, и
         поэтому стремимся предложить только самые качественные и стильные товары
         для всех любителей скейтбординга.
