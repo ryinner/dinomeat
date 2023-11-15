ee4a2073caa99b098777ccee3dffa8a9b8ca5715
ryinner <vinograd18062002@gmail.com>
2023-11-15 16:54:53 +0500

    employers start

--- components/Index/TheEmployers.tsx
+++ components/Index/TheEmployers.tsx
@@ -1,3 +1,19 @@
+import styles from "./TheEmployers.module.scss";
+
+const employers = [{ id: 1 }];
+
 export default function TheEmployers() {
-  return <section></section>
+  return (
+    <section className={styles.employers}>
+      <div className={styles.employers__heading}>Наши сотрудники</div>
+      <div className={styles.employers__container}>
+        <div className={styles.employers__borders} />
+        <ul className={styles.employers__list}>
+          {employers.map((e) => (
+            <li key={e.id}>{e.id}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
