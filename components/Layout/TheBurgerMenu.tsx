7ce7979d683f390e35c628ff5a30f7e9e9e9e5c0
ryinner <vinograd18062002@gmail.com>
2023-11-17 11:27:04 +0500

    burger menu

--- components/Layout/TheBurgerMenu.tsx
+++ components/Layout/TheBurgerMenu.tsx
@@ -7,7 +7,7 @@ import Burger from "../../public/icons/burger.svg";
 import CatalogCategoryLink from "../Links/CatalogCategoryLink";
 import { TheNavigationCategoriesContext } from "../TheProviders/TheNavigationCategoriesContext";
 import styles from "./TheBurgerMenu.module.scss";
-import TheSearch from './TheSearch';
+import TheSearch from "./TheSearch";
 
 const variants = {
   open: {
@@ -36,6 +36,7 @@ export default function TheBurgerMenu() {
         onClick={toggleHandler}
       />
       <motion.div
+        initial={"closed"}
         animate={isOpen ? "open" : "closed"}
         variants={variants}
         transition={{
@@ -43,20 +44,20 @@ export default function TheBurgerMenu() {
         }}
         className={styles["burger-menu__sidebar"]}
       >
-        <div className={styles["burger-menu__header"]}>
-          <Image
-            src={Burger}
-            height={22}
-            alt="Открыть навигацию"
-            className={`${styles["burger-menu__icon"]} ${styles["burger-menu__icon--active"]}`}
-            onClick={toggleHandler}
-          />
-          <TheSearch className={styles["burger-menu__search"]} />
-        </div>
         <nav>
-          <ul>
+          <ul className={styles["burger-menu__list"]}>
+            <li className={`${styles["burger-menu__header"]} ${styles["burger-menu__item"]}`}>
+              <Image
+                src={Burger}
+                height={22}
+                alt="Открыть навигацию"
+                className={`${styles["burger-menu__icon"]} ${styles["burger-menu__icon--active"]}`}
+                onClick={toggleHandler}
+              />
+              <TheSearch className={styles["burger-menu__search"]} />
+            </li>
             {categories.map((c) => (
-              <li key={c.id}>
+              <li key={c.id} className={styles["burger-menu__item"]}>
                 <CatalogCategoryLink category={c} />
               </li>
             ))}
