bc7595c25fef634808103571f4c693fafced169b
ryinner <vinograd18062002@gmail.com>
2023-11-17 08:50:43 +0500

    burger menu

--- components/Layout/TheBurgerMenu.tsx
+++ components/Layout/TheBurgerMenu.tsx
@@ -1,27 +1,28 @@
 "use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import Image from "next/image";
 import { useContext, useState } from "react";
 import Burger from "../../public/icons/burger.svg";
 import CatalogCategoryLink from "../Links/CatalogCategoryLink";
 import { TheNavigationCategoriesContext } from "../TheProviders/TheNavigationCategoriesContext";
 import styles from "./TheBurgerMenu.module.scss";
+import TheSearch from './TheSearch';
 
 const variants = {
   open: {
-    x: 0
+    x: 0,
   },
   closed: {
-    x: -500
-  }
-}
+    x: -500,
+  },
+};
 
 export default function TheBurgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
   const categories = useContext(TheNavigationCategoriesContext);
 
-  function toggleHandler () {
+  function toggleHandler() {
     setIsOpen((isOpen) => !isOpen);
   }
 
@@ -34,25 +35,34 @@ export default function TheBurgerMenu() {
         className={styles["burger-menu__icon"]}
         onClick={toggleHandler}
       />
-      
-        <motion.div
-          animate={isOpen ? 'open' : 'closed'}
-          variants={variants}
-          transition={{
-            duration: 0.3,
-          }}
-          className={styles["burger-menu__sidebar"]}
-        >
-          <nav>
-            <ul>
-              {categories.map((c) => (
-                <li key={c.id}>
-                  <CatalogCategoryLink category={c} />
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </motion.div>
+      <motion.div
+        animate={isOpen ? "open" : "closed"}
+        variants={variants}
+        transition={{
+          duration: 0.3,
+        }}
+        className={styles["burger-menu__sidebar"]}
+      >
+        <div className={styles["burger-menu__header"]}>
+          <Image
+            src={Burger}
+            height={22}
+            alt="Открыть навигацию"
+            className={`${styles["burger-menu__icon"]} ${styles["burger-menu__icon--active"]}`}
+            onClick={toggleHandler}
+          />
+          <TheSearch className={styles["burger-menu__search"]} />
+        </div>
+        <nav>
+          <ul>
+            {categories.map((c) => (
+              <li key={c.id}>
+                <CatalogCategoryLink category={c} />
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </motion.div>
     </div>
   );
 }
--- components/Layout/TheSearch.tsx
+++ components/Layout/TheSearch.tsx
@@ -1,23 +1,30 @@
 'use client';
 
 import Image from 'next/image';
-import { useRef, type FormEvent } from "react";
+import React, { useRef, type FormEvent } from "react";
 import GoSearch from '../../public/icons/go-search.svg';
 import Search from '../../public/icons/search.svg';
 import styles from './TheSearch.module.scss';
 
-export default function TheSearch() {
+export default function TheSearch({ goSearchIcon = GoSearch, className, searchIcon = Search, ...divProps }: Props) {
   const searchInput = useRef(null);
+
+  const classNameComputed = `${styles.search} ${className === undefined ? '' : className}`;
 
   async function inputHandler(e: FormEvent<HTMLInputElement>) {
     e.preventDefault();
   }
 
   return (
-    <search className={styles.search} role="search">
-      <Image className={styles['search__icon-magnifier']} src={Search} alt='Иконка поиска' />
+    <search className={classNameComputed} role="search" {...divProps}>
+      <Image className={styles['search__icon-magnifier']} src={searchIcon} alt='Иконка поиска' />
       <input className={styles.search__input} placeholder='Поиск' ref={searchInput} type="text" onChange={inputHandler} />
-      <Image className={styles['search__icon-go']} src={GoSearch} alt='Найти' />
+      <Image className={styles['search__icon-go']} src={goSearchIcon} alt='Найти' />
     </search>
   );
+}
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  searchIcon?: string;
+  goSearchIcon?: string;
 }
