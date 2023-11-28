19c6189fd3c0ab386f1edb3241f35fd2cbf3ff70
ryinner <vinograd18062002@gmail.com>
2023-11-28 17:17:37 +0500

    work with catalog

--- components/Links/CatalogProductLink.tsx
+++ components/Links/CatalogProductLink.tsx
@@ -2,10 +2,11 @@ import { Product } from '@prisma/client';
 import Link from "next/link";
 import React from 'react';
 
-export default function CatalogProductLink({ product: { slug, name }, ...props }: Props) {
-  return <Link href={`/catalog/${slug}`}><span {...props}>{name}</span></Link>;
+export default function CatalogProductLink({ product: { slug, name }, children, ...props }: Props) {
+  return <Link href={`/catalog/${slug}`}><span {...props}>{ children ?? name}</span></Link>;
 }
 
 interface Props extends React.LinkHTMLAttributes<HTMLLinkElement> {
   product: Pick<Product, 'name' | 'slug'>
+  children: React.ReactNode;
 }
