8a8def267daf682b1a70a4ee94b0504afab0a8d1
ryinner <vinograd18062002@gmail.com>
2023-11-29 13:06:33 +0500

    product

--- components/Catalog/TheProducts.tsx
+++ components/Catalog/TheProducts.tsx
@@ -11,8 +11,9 @@ export default function TheProducts ({ products }: Props) {
             <Image src={getUrl(p.images[0].image.url)} alt={p.images[0].image.alt ?? ''} fill={true} className={styles.products__image} />
           </picture>
           <div className={styles.products__info}>
-            <div className={styles.products__name}>{p.name} {p.article}</div>
-            <div className={styles.products__price}>Цена: {p.price} ₽</div>
+            <div className={styles.products__name}>{p.name}</div>
+            <div className={styles.products__article}>{p.article}</div>
+            <div className={styles.products__price}><span>Цена</span> <span className={styles['products__price-value']}><span>{p.price}</span> <span>руб.</span></span></div>
           </div>
         </article>
       ))}
