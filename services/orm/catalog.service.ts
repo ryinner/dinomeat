04c59eb47a37bdc4c0f37bc7411c5fa7be4731e9
ryinner <vinograd18062002@gmail.com>
2023-11-29 13:26:37 +0500

    styles

--- services/orm/catalog.service.ts
+++ services/orm/catalog.service.ts
@@ -2,7 +2,7 @@ import { Prisma } from "@prisma/client";
 import { prisma } from "../lib/prisma.service";
 import { pagination } from "./pagination.service";
 
-const LIMIT = 1;
+const LIMIT = 16;
 
 export async function catalog({
   page = 1,
