b4a1340fa55fbaf42709eb07a6324af17a7db158
ryinner <vinograd18062002@gmail.com>
2023-12-01 09:41:06 +0500

    base contacts

--- app/(index)/contacts/page.tsx
+++ app/(index)/contacts/page.tsx
@@ -1,10 +1,23 @@
-import { Metadata } from 'next';
+import { Metadata } from "next";
+import styles from "./page.module.scss";
 
 export const metadata: Metadata = {
   title: "Контакты | Dinomeät",
   description: "Большой ассортимент в интернет-магазине dinomeät. Контакты.",
 };
 
-export default function Contacts () {
-  return <></>
+export default function Contacts() {
+  return (
+    <section className={styles.contacts}>
+      <div className={styles.contacts__info}>
+        <div className={styles.contacts__text}>
+          <div>
+            Наш офис находится в обл. Челябинская, г. Челябинск, ул.
+            Энтузиастов, д. 12, офис 207
+          </div>
+          <div className={styles['contacts__work-time']}>Время работы: с 11:00 до 19:00 </div>
+        </div>
+      </div>
+    </section>
+  );
 }
