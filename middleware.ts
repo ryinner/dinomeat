170afce01cd2404bcbffd5b65dbd03a8ba626a1e
ryinner <vinograd18062002@gmail.com>
2023-12-04 12:08:48 +0500

    auth prepare

--- middleware.ts
+++ middleware.ts
@@ -7,8 +7,13 @@ export default withAuth(
         if (process.env.NODE_ENV !== 'production') {
           return true;
         }
-        if (req.nextUrl.href.includes('/admin')) {
+        if (req.nextUrl.href.includes('/admin') || req.nextUrl.href.includes('/profile')) {
           if (token === null) {
+            return false;
+          }
+        }
+        if (req.nextUrl.href.includes('/auth')) {
+          if (token !== null) {
             return false;
           }
         }
