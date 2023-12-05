ce248eaf615ea2d6fcb92e14078364df03d281d4
ryinner <vinograd18062002@gmail.com>
2023-12-05 10:07:43 +0500

    auth

--- components/Forms/VkAuthButton.tsx
+++ components/Forms/VkAuthButton.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import { signIn } from 'next-auth/react';
+import { useSearchParams } from 'next/navigation';
 import Button from '../Button/Button';
 
 export default function VkAuthButton () {
-  return  <Button onClick={() => signIn('vk')}>Войти через VK</Button>
+  const searchParams = useSearchParams();
+
+  return  <Button onClick={() => signIn('vk', { redirect: false, callbackUrl: searchParams.get('redirectUri') ?? '/profile' })}>Войти через VK</Button>
 }
