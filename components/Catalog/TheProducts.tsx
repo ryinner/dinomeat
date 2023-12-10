c92849b26b1828b97fec4427f49d404709546563
Ryinner <vinograd18062002@gmail.com>
2023-12-10 22:52:35 +0500

    DOM FIX

--- components/Catalog/TheProducts.tsx
+++ components/Catalog/TheProducts.tsx
@@ -1,28 +1,42 @@
-import type { ProductCatalog } from '@/@types/private';
-import { getUrl } from '@/services/lib/image.service';
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from './TheProducts.module.scss';
+import type { ProductCatalog } from "@/@types/private";
+import { getUrl } from "@/services/lib/image.service";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./TheProducts.module.scss";
 
-export default function TheProducts ({ products }: Props) {
-  return <section className={styles.products}>
-    {products.map((p) => (
-      <Link className={styles.products__item} key={p.id} href={`/catalog/${p.slug}`}>
-        <article className={styles.products__container}>
+export default function TheProducts({ products }: Props) {
+  return (
+    <section className={styles.products}>
+      {products.map((p) => (
+        <article
+          key={p.id}
+          className={`${styles.products__container} ${styles.products__item}`}
+        >
           <picture className={styles.products__picture}>
-            <Image src={getUrl(p.images[0].image.url)} alt={p.images[0].image.alt ?? ''} fill={true} className={styles.products__image} />
+          <Link href={`/catalog/${p.slug}`}>
+            <Image
+              src={getUrl(p.images[0].image.url)}
+              alt={p.images[0].image.alt ?? ""}
+              fill={true}
+              className={styles.products__image}
+            />
+            </Link>
           </picture>
           <div className={styles.products__info}>
-            <div className={styles.products__name}>{p.name}</div>
-            <div className={styles.products__article}>{p.article}</div>
-            <div className={styles.products__price}><span>Цена</span> <span>{p.price} ₽</span></div>
+            <div className={styles.products__name}>
+              <Link href={`/catalog/${p.slug}`}>{p.name}</Link>
+            </div>
+            <div className={styles.products__article}><Link href={`/catalog/${p.slug}`}>{p.article}</Link></div>
+            <div className={styles.products__price}>
+              <span>Цена</span> <span>{p.price} ₽</span>
+            </div>
           </div>
         </article>
-      </Link>
       ))}
-  </section>
+    </section>
+  );
 }
 
 interface Props {
-  products: ProductCatalog[]
+  products: ProductCatalog[];
 }
