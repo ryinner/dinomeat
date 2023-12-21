c51250f439a470b6be98f2d5e63f191d8d26784f
ryinner <vinograd18062002@gmail.com>
2023-12-21 13:29:21 +0500

    the cart default skelet

--- components/Cart/TheCart.tsx
+++ components/Cart/TheCart.tsx
@@ -3,6 +3,7 @@
 import { ProductCart } from "@/@types/private";
 import { frontRequest } from "@/services/api/api.service";
 import { useEffect, useRef, useState } from "react";
+import DefaultLink from '../Links/DefaultLink';
 import { useCart } from "../TheProviders/TheCartContext";
 import CartProduct from "./CartProduct";
 import styles from "./TheCart.module.scss";
@@ -12,6 +13,8 @@ export default function TheCart() {
   const [products, setProducts] = useState<ProductCart[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const isRequested = useRef(false);
+
+  const cartSum = cart.reduce((sum, cartItem) => sum + cartItem.amount * (products.find(p => p.id === cartItem.id)?.product?.price ?? 0), 0);
 
   const isCartEmpty = cart.length === 0;
 
@@ -44,10 +47,29 @@ export default function TheCart() {
         <p>Корзина пуста, добавьте товар</p>
       ) : (
         <div>
-          {products.map((p) => (
-            <CartProduct key={p.id} product={p} />
-          ))}
-          Корзина находится в разработке! Скоро вы сможете сделать заказ :)
+          <div className={styles.cart__header}>
+            <div>Оформить заказ</div>
+            <div>Товары</div>
+          </div>
+          <div className={styles.cart__content}>
+            <div>
+              {/* Форма */}
+            </div>
+            <div>
+              {products.map((p) => (
+                <CartProduct key={p.id} product={p} />
+              ))}
+            </div>
+          </div>
+          <div className={styles.cart__footer}>
+            <div className={styles.cart__total}>
+              <span>К оплате:</span>
+              <span className={styles.cart__price}>{cartSum} ₽</span>
+            </div>
+            <div className={styles.cart__buttons}>
+              <DefaultLink href='/catalog'>Каталог</DefaultLink>
+            </div>
+          </div>
         </div>
       )}
     </div>
