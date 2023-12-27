d8d6b00bbc44c84e993837dc2fe227659656f050
ryinner <vinograd18062002@gmail.com>
2023-12-27 10:28:34 +0500

    start banners && orders

---
+++ app/(admin)/admin/banners/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Баннеры",
+  description: "Страница перечисления всех баннеров",
+};
+
+export default function BannersList () {
+  return <>
+  </>
+}
---
+++ app/(admin)/admin/orders/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Заказы",
+  description: "Страница перечисления всех заказов",
+};
+
+export default function OrdersList () {
+  return <>
+  </>
+}
