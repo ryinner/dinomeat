3dba0b0f08623a2e4ae322b9ab64ed16dbd03310
Ryinner <vinograd18062002@gmail.com>
2024-01-05 23:18:51 +0500

    order page

--- components/Order/OrderProducts.tsx
+++ components/Order/OrderProducts.tsx
@@ -1,7 +1,25 @@
 import { OrdersProductsSizesEdit } from '@/@types/private';
+import { getUrl } from '@/services/lib/image.service';
+import Image from 'next/image';
+import styles from './OrderProducts.module.scss';
 
 export default function OrderProducts ({ sizes }: Props) {
   return <section>
+    {sizes.map(s => <article className={styles.product} key={s.id}>
+      <picture className={styles.product__image}>
+        <Image
+          src={getUrl(s.productSize.product.images[0].image.url)}
+          alt={s.productSize.product.images[0].image.alt ?? ''}
+          fill={true}
+          quality={100}
+        />
+      </picture>
+      <div className={styles.product__info}>
+        <div className={styles.product__paragraph}>{s.productSize.product.name}</div>
+        <div className={styles.product__paragraph}>Размер: {s.productSize.size.name}</div>
+        <div className={styles.product__paragraph}>Кол-во: {s.amount}</div>
+      </div>
+    </article>)}
   </section>
 }
 
