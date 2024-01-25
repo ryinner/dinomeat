94c24051341f9fc3534e14ea97a0c068c8036786
ryinner <vinograd18062002@gmail.com>
2024-01-25 12:13:58 +0500

    new url

--- services/api/alpha.service.ts
+++ services/api/alpha.service.ts
@@ -1,6 +1,7 @@
 import { request } from './api.service';
 
-const BASE_URL = 'https://alfa.rbsuat.com/payment/';
+// const BASE_URL = 'https://alfa.rbsuat.com/payment/';
+const BASE_URL = 'https://pay.alfabank.ru/payment/';
 
 /**
  * @function 
